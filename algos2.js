729f2eb002eb94e06d0036b1deacd48789f0d4e0
Wade Hunt <wadehuntdev@gmail.com>
2024-04-21 14:33:23 -0700

    Add Factorials and threesFives functions

--- algos2.js
+++ algos2.js
@@ -11,3 +11,32 @@ function sigma(num){
     console.log(sum)
 }
 // sigma(9)
+
+// Write a function factorial(num)​ that, given a
+// number, returns the product (multiplication) of all
+// positive integers from 1 up to number (inclusive).
+function factorial(num){
+    let product = 1;
+    for(i=1; i<=num; i++){
+        product = product * i;
+    }
+    console.log(product)
+}
+// factorial(5)
+
+// Create function ThreesFives()​ that adds each value from 100 and 4000000 (inclusive) if that value
+// is evenly divisible by 3 or 5 but not both. Display the final sum in the console.
+function threesFives(){
+    let sum = 0;
+    for (i=100 ; i <=4000000 ; i++){
+        if(i % 3 === 0 || i % 5 === 0 ){
+            if(i % 3 === 0 && i % 5 === 0){
+                break
+            }
+        sum = sum + i;
+        }
+    }
+    console.log(sum)
+}
+threesFives()
+
