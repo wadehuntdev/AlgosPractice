7904ab05960b150ae576faec24fdd52304cdf6f9
Wade Hunt <wadehuntdev@gmail.com>
2024-04-28 11:12:40 -0700

    Finish popFront and removeAt Functions

--- algos3.js
+++ algos3.js
@@ -25,3 +25,35 @@ function insertAt(arr, idx , value){
     console.log(arr)
 }
 // insertAt([1,3,6,8], 2,66)
+
+// Given an array, remove and return the value at
+// the beginning of the array. Do this without using
+// any built-in array methods except pop()​.
+function popFront(arr){
+    let temp = arr[arr.length-1];
+    arr.pop()
+    console.log(arr)
+    let i = arr.length;
+    while( i > 0){
+        arr[i] = arr[i-1];
+        i--;
+    }
+    arr[0] = temp
+    console.log(arr)
+}
+// popFront([1,4,6,9,5,3])
+
+// Given an array and an index into the array,
+// remove and return the array value at that index.
+// Do this without using any built-in array methods
+// except pop()​. Think of PopFront(arr)​ as
+// equivalent to RemoveAt(arr,0)
+function removeAt(arr,value){
+    let i = arr.length-1;
+    while(i > value){
+            arr[i-1] = arr[i] 
+            i--;
+    }
+    console.log(arr)
+}
+removeAt([1,3,5,7,9,11,13,15], 4)
